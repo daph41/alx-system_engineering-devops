adc58edf648062b25e2e17fd1227c8cf7c78405d
daph41 <daphinemoraa@gmail.com>
2023-08-02 04:24:08 -0700

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
-"""
-returns information about his/her TODO list progress for a
-given employee ID using a REST API
-"""
+""" Contains a python script that, using the JSONplaceholder API, for a given
+    employee ID, returns information about employees todo list progress """
 import csv
 import requests
 from sys import argv
 
 
-def get_todo_csv():
-    """returns employee's TODO list progress"""
-    r_user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                          .format(argv[1]))
-
-    r_todo = requests.get('https://jsonplaceholder.typicode.com/todos?'
-                          'userId={}'.format(argv[1]))
-    try:
-        user_dict = r_user.json()
-        task_dict = r_todo.json()
-        with open("{}.csv".format(argv[1]), "w") as csvfile:
-            f = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
-            for task in task_dict:
-                f.writerow([user_dict["id"], user_dict["username"],
-                            task["completed"], task["title"]])
-    except:
-        pass
-if __name__ == "__main__":
-    get_todo_csv()
+if __name__ == '__main__':
+    userId = argv[1]
+    name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(userId)).json()
+    # todo variable = grabs all todos (completed or not) for the user passed in
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                        .format(userId)).json()
+    with open("{}.csv".format(userId), 'w') as acsvfile:
+        csvwriter = csv.writer(acsvfile, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            csvwriter.writerow([userId, name.get('username'),
+                                task.get('completed'), task.get('title')])
