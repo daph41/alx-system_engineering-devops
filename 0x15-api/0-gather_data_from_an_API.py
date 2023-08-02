69c6246e8fafa2d0235888fa8048b37a552554b3
daph41 <daphinemoraa@gmail.com>
2023-08-02 04:20:41 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-"""
-returns information about his/her TODO list progress for a
-given employee ID using a REST API
-"""
+""" Python script that, using this REST API, for a given employee ID, returns
+    information about his/her TODO list progress. Requirements:
+    You must use urllib or requests module
+    The script must accept an integer as a parameter, which is the employee ID
+    The script must display on the standard output the employee TODO list...
+    ...progress in this exact format: First line: Employee EMPLOYEE_NAME is...
+    ...done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):,
+    NUMBER_OF_DONE_TASKS: number of completed tasks, TOTAL_NUMBER_OF_TASKS:...
+    ...total number of tasks, which is the sum of completed and non-completed..
+    ..tasks Second and N next lines display the title of completed tasks:..
+    ..TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE) """
 import requests
 from sys import argv
 
 
-def get_todo():
-    """returns employee's TODO list progress"""
-    r_user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                          .format(argv[1]))
-
-    r_todo_true = requests.get('https://jsonplaceholder.typicode.com/todos?'
-                               'userId={}&&completed=true'.format(argv[1]))
-    r_todo_false = requests.get('https://jsonplaceholder.typicode.com/todos?'
-                                'userId={}&&completed=false'.format(argv[1]))
-    try:
-        user_dict = r_user.json()
-        true_todo_dict = r_todo_true.json()
-        false_todo_dict = r_todo_false.json()
-        print("Employee {} is done with tasks({}/{}):".
-              format(user_dict['name'], len(true_todo_dict),
-                     len(false_todo_dict) + len(true_todo_dict)))
-        for task in true_todo_dict:
-            print("\t {}".format(task['title']))
-    except:
-        pass
 if __name__ == "__main__":
-    get_todo()
+    userId = argv[1]
+    name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(userId)).json()
+    # todo variable = grabs all todos (completed or not) for the user passed in
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                        .format(userId)).json()
+    completed = []
+    for task in todo:
+        if task.get('completed') is True:
+            completed.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(name.get('name'), len(completed), len(todo)))
+    for task in completed:
+        print("\t {}".format(task))
